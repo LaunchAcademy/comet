30d7fc9ac59356398c21597abc63567fff3f200f
Adam Sheehan <a.t.sheehan@gmail.com>
2014-01-02 12:36:36 -0500

    Write config file with placeholder keys.

--- lib/mir/init.rb
+++ lib/mir/init.rb
@@ -1,9 +1,16 @@
+require 'yaml'
+
 module Mir
   class Init
     class << self
       def init_project_dir(dirname)
         config_file = File.join(dirname, '.mir')
-        File.write(config_file, 'test')
+        config = {
+          'email' => 'EMAIL GOES HERE...',
+          'token' => 'TOKEN GOES HERE...'
+        }
+
+        File.write(config_file, config.to_yaml)
       end
     end
   end
--- spec/lib/mir/init_spec.rb
+++ spec/lib/mir/init_spec.rb
@@ -1,5 +1,6 @@
 require 'rspec'
 require 'tmpdir'
+require 'yaml'
 require 'mir'
 
 describe Mir::Init do
@@ -11,6 +12,18 @@ describe Mir::Init do
 
         Mir::Init.init_project_dir(dir)
         expect(File.exists?(config_file)).to be_true
+      end
+    end
+
+    it 'creates placeholders for the user email and token' do
+      Dir.mktmpdir do |dir|
+        Mir::Init.init_project_dir(dir)
+
+        config_file = File.join(dir, '.mir')
+        config = YAML.load(File.read(config_file))
+
+        expect(config.keys).to include('email')
+        expect(config.keys).to include('token')
       end
     end
   end
