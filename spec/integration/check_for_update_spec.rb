7178b5a3862abe6397e582a53a617b722a60377f
Adam Sheehan <a.t.sheehan@gmail.com>
2014-03-11 10:24:46 -0400

    Add more tests for checking for updates.
    Verify that the user isn't presented with an update notice if they
    have the latest version of the gem.

--- spec/integration/check_for_update_spec.rb
+++ spec/integration/check_for_update_spec.rb
@@ -40,6 +40,11 @@ describe 'check for update' do
     FileUtils.remove_entry_secure(tmpdir) if Dir.exists?(tmpdir)
   end
 
+  let(:update_notice) do
+    'NOTICE: An updated version of comet exists. ' +
+      'Run `gem update comet` to upgrade.'
+  end
+
   context 'when a newer version exists' do
     let(:next_version) do
       Comet::Version.build(
@@ -52,19 +57,26 @@ describe 'check for update' do
       Comet::API.stub(:latest_gem_version).and_return(next_version)
     end
 
-    let(:notice) do
-      'NOTICE: An updated version of comet exists. ' +
-        'Run `gem update comet` to upgrade.'
-    end
-
     it 'notifies user to update the gem' do
       stdout, stderr = capture_output { Comet::Runner.go(['list'], tmpdir) }
-      expect(stderr).to include(notice)
+      expect(stderr).to include(update_notice)
     end
 
     it 'does not notify the user when running a test suite' do
       stdout, stderr = capture_output { Comet::Runner.go(['test'], tmpdir) }
-      expect(stderr).to_not include(notice)
+      expect(stderr).to_not include(update_notice)
     end
+  end
+
+  context 'when the current version is up-to-date' do
+    before :each do
+      Comet::API.stub(:latest_gem_version).and_return(Comet::VERSION)
+    end
+
+    it 'does not notify user to update the gem' do
+      stdout, stderr = capture_output { Comet::Runner.go(['list'], tmpdir) }
+      expect(stderr).to_not include(update_notice)
+    end
+
   end
 end
